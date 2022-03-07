0ee3acb5a14d33eb67c6793b56e52907673eefaf
javasBC <98883189+javasBC@users.noreply.github.com>
2022-03-07 10:31:58 +0200

    Update sqlN:M.sql

--- sqlN:M.sql
+++ sqlN:M.sql
@@ -30,3 +30,13 @@ CREATE TABLE ActInMovRel(
 	FOREIGN KEY(movieId) REFERENCES Movies(id),
 	FOREIGN KEY(actorId) REFERENCES Actors(id)	
 );
+
+
+INSERT INTO Actors 
+(name , birthDate,firstMovie,cityOfBirth,isMarried)
+VALUES 
+("Wil SMith","25-9-1968","MEN In BLack","Philadelphia","YES"),
+("Denzel washington","28-12-1954","Training day","Philadelphia","YES"),
+("chris hemsworth","11-8-1983","Thor","sydney","YES"),
+("chris Tucker","31-08-1971","Rush Hour","georgia","no"),
+("gal gadot","30-04-1985","Red Notice","rosh ha'yen","YES")
