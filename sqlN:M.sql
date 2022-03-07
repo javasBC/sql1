7afb11a41474f9a0c849aa0b07e936426e2e8529
javasBC <98883189+javasBC@users.noreply.github.com>
2022-03-07 10:18:11 +0200

    Update sqlN:M.sql

--- sqlN:M.sql
+++ sqlN:M.sql
@@ -1,1 +1,32 @@
+CREATE TABLE Actors(
+	id INTEGER ,
+	name TEXT,
+	birthDate TEXT DEFAULT "1,1,1900",
+	firstMovie TEXT,
+	cityOfBirth TEXT,
+	isMarried TEXT DEFAULT "NO",
+	PRIMARY KEY(id AUTOINCREMENT)
+);
 
+///////////////////////////////////////////
+
+CREATE TABLE Movies(
+	id INTEGER ,
+	title TEXT,
+	len INTEGER,
+	year INTEGER ,
+	cost INTEGER,
+	genre TEXT ,
+	PRIMARY KEY(id AUTOINCREMENT)
+);
+
+
+//////////////////////////////////////////
+
+CREATE TABLE ActInMovRel(
+	movieId INTEGER,
+	actorId INTEGER,
+	PRIMARY KEY(actorId,movieId),
+	FOREIGN KEY(movieId) REFERENCES Movies(id),
+	FOREIGN KEY(actorId) REFERENCES Actors(id)	
+);
